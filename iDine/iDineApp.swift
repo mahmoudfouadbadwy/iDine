b5071c8a85487e4a47295f8998288eb17b6e4b04
Mahmoud Fouad <mahmoudfouad443@gmail.com>
2021-04-12 15:05:03 +0200

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Mahmoud Fouad on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
